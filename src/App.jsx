531407d63ee50ae84f0ebc37a672f1a69e786f89
DigiHoss <hossamelhamdi6@gmail.com>
2025-06-17 18:29:42 +0100

    bg is resolved

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import "./index.css";
 import WelcomeScreen from "./components/WelcomeScreen";
-
 import Home from "./Pages/Home";
-
 import About from "./Pages/About";
-
 import Navbar from "./components/Navbar";
 import Portfolio from "./Pages/Portfolio";
 import Contact from "./Pages/Contact";
@@ -22,6 +19,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
       </AnimatePresence>
       {!showWelcome && (
         <>
+          
            <Navbar />
           <Home />
           <About />
@@ -52,9 +50,9 @@ const ProjectPageLayout = () => (
       <center>
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-          © 2023{" "}
+          © 2025{" "}
           <a href="https://flowbite.com/" className="hover:underline">
-            EkiZR™
+          DigiHoss™
           </a>
           . All Rights Reserved.
         </span>
@@ -79,7 +77,7 @@ function App() {
           }
         />
         <Route path="/project/:id" element={<ProjectPageLayout />} />
-
+      
       </Routes>
     </BrowserRouter>
   );
